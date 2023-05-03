5caa8da5052a9740e088cf7f5a03523beba8108e
Nguyen Quy <dinhquy4869@gmail.com>
2023-05-03 21:45:14 +0700

    fix

---
+++ Web_CuaHangCafe/Areas/Admin/Controllers/AccountsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web_CuaHangCafe.Models;
+using Web_CuaHangCafe.Models.Authentication;
+using X.PagedList;
+
+namespace Web_CuaHangCafe.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Accounts")]
+    public class AccountsController : Controller
+    {
+        QlcuaHangCafeContext db = new QlcuaHangCafeContext();
+
+        [Route("")]
+        [Route("Index")]
+        [Authentication]
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 30;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var listItem = db.TbQuanTriViens.AsNoTracking().OrderBy(x => x.TenNguoiDung).ToList();
+            PagedList<TbQuanTriVien> pagedListItem = new PagedList<TbQuanTriVien>(listItem, pageNumber, pageSize);
+            return View(pagedListItem);
+        }
+
+        [Route("Create")]
+        [Authentication]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("Create")]
+        [Authentication]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TbQuanTriVien quanTriVien)
+        {
+            db.TbQuanTriViens.Add(quanTriVien);
+            db.SaveChanges();
+            TempData["Message"] = "Thêm thành công";
+            return RedirectToAction("Index", "Accounts");
+        }
+
+        [Route("Edit")]
+        [Authentication]
+        [HttpGet]
+        public IActionResult Edit(string id, string name)
+        {
+            var quanTriVien = db.TbQuanTriViens.Find(id);
+            ViewBag.id = id;
+            return View(quanTriVien);
+        }
+
+        [Route("Edit")]
+        [Authentication]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(TbQuanTriVien quanTriVien)
+        {
+            db.Entry(quanTriVien).State = EntityState.Modified;
+            db.SaveChanges();
+            TempData["Message"] = "Sửa thành công";
+            return RedirectToAction("Index", "Accounts");
+        }
+
+        [Route("Delete")]
+        [Authentication]
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            TempData["Message"] = "";
+            db.Remove(db.TbQuanTriViens.Find(id));
+            db.SaveChanges();
+            TempData["Message"] = "Xoá thành công";
+            return RedirectToAction("Index", "Accounts");
+        }
+    }
+}
--- Web_CuaHangCafe/Controllers/ProductsController.cs
+++ Web_CuaHangCafe/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Web_CuaHangCafe.Models;
 using X.PagedList;
 using Web_CuaHangCafe.Helpers;
+using System.Drawing.Printing;
 
 namespace Web_CuaHangCafe.Controllers
 {
@@ -11,33 +12,29 @@ namespace Web_CuaHangCafe.Controllers
     {
         QlcuaHangCafeContext db = new QlcuaHangCafeContext();
 
-        public IActionResult Index(int? page, string target, string targetname)
+        public IActionResult Index(int? page)
+        {
+            int pageSize = 18;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var listItem = db.TbSanPhams.AsNoTracking().OrderBy(x => x.MaSanPham).ToList();
+            PagedList<TbSanPham> pagedListItem = new PagedList<TbSanPham>(listItem, pageNumber, pageSize);
+            return View(pagedListItem);
+        }
+
+        public IActionResult ProductType(string target, int? page)
         {
             int pageSize = 9;
             int pageNumber = page == null || page < 0 ? 1 : page.Value;
-
-            if (target == "all" || target == "")
-            {
-                var listItem = db.TbSanPhams.AsNoTracking().OrderBy(x => x.MaSanPham).ToList();
-                PagedList<TbSanPham> pagedListItem = new PagedList<TbSanPham>(listItem, pageNumber, pageSize);
-                ViewBag.target = target;
-                ViewBag.targetname = targetname;
-                return View(pagedListItem);
-            }
-            else
-            {
-                var listItem = db.TbSanPhams.AsNoTracking().Where(x => x.MaNhomSp == target).OrderBy(x => x.TenSanPham).ToList();
-                PagedList<TbSanPham> pagedListItem = new PagedList<TbSanPham>(listItem, pageNumber, pageSize);
-                ViewBag.target = target;
-                ViewBag.targetname = targetname;
-                return View(pagedListItem);
-            }
+            var listItem = db.TbSanPhams.AsNoTracking().Where(x => x.MaNhomSp == target).OrderBy(x => x.TenSanPham).ToList();
+            PagedList<TbSanPham> pagedListItem = new PagedList<TbSanPham>(listItem, pageNumber, pageSize);
+            ViewBag.target = target;
+            return View(pagedListItem);
         }
 
-        public IActionResult Details(string maSanPham)
+        public IActionResult Details(string id)
         {
-            var sanPham = db.TbSanPhams.SingleOrDefault(x => x.MaSanPham == maSanPham);
-            return View(sanPham);
+            var products = db.TbSanPhams.SingleOrDefault(x => x.MaSanPham == id);
+            return View(products);
         }
     }
 }
